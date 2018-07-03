d86a82842d595911c73b64639169c88122fba6d4
Florian Stabel <florian.stabel@gmail.com>
2018-07-03 14:12:40 +0200

    asdf

--- KeePassTestSuite/Recordings/Login.cs
+++ KeePassTestSuite/Recordings/Login.cs
@@ -79,16 +79,16 @@ namespace KeePassTestSuite.Recordings
 
             Init();
 
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'LogInDialog200.MasterPassword' at Center.", repo.LogInDialog200.MasterPasswordInfo, new RecordItemIndex(0));
-            repo.LogInDialog200.MasterPassword.Click(100);
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'LogInDialog.MasterPassword' at Center.", repo.LogInDialog.MasterPasswordInfo, new RecordItemIndex(0));
+            repo.LogInDialog.MasterPassword.Click(100);
             Delay.Milliseconds(0);
             
-            Report.Log(ReportLevel.Info, "Keyboard", "Key sequence '●●' with focus on 'LogInDialog200.MasterPassword'.", repo.LogInDialog200.MasterPasswordInfo, new RecordItemIndex(1));
-            repo.LogInDialog200.MasterPassword.PressKeys("rx");
+            Report.Log(ReportLevel.Info, "Keyboard", "Key sequence '●●' with focus on 'LogInDialog.MasterPassword'.", repo.LogInDialog.MasterPasswordInfo, new RecordItemIndex(1));
+            repo.LogInDialog.MasterPassword.PressKeys("rx");
             Delay.Milliseconds(0);
             
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'LogInDialog200.BtOK' at Center.", repo.LogInDialog200.BtOKInfo, new RecordItemIndex(2));
-            repo.LogInDialog200.BtOK.Click(100);
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'LogInDialog.BtOK' at Center.", repo.LogInDialog.BtOKInfo, new RecordItemIndex(2));
+            repo.LogInDialog.BtOK.Click(100);
             Delay.Milliseconds(0);
             
         }
